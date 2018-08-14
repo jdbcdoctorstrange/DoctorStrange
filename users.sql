f3d2bff05e1796c1651cd7a3a38f0c3941be4bb6
GBTC441016UR <GBTC441016UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-08-14 13:56:53 -0400

    adding sample sql queries

--- users.sql
+++ users.sql
@@ -1,7 +1,8 @@
--- select * from user;
+select * from user;
+select * from role;
 
--- select * from role;
-
+-- get all users and their roles
+-- The many-to-many relationship between user and role creates a third table in the database called user_roles
 select username, password, role
 from user a
 inner join user_roles b on a.id = b.user_id
